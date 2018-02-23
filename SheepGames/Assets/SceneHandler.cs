6194b21140c96429f0ee94c6bdda71e06e7623c1
Vera <schoenigervera@gmail.com>
2018-02-23 10:26:12 +0100

    Intro
    - Intro now has H, correct Font, background and better Timing and also loads to main menu next

--- SheepGames/Assets/SceneHandler.cs
+++ SheepGames/Assets/SceneHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour {
+public class SceneHandler : MonoBehaviour {
 
     private void Update()
     {
@@ -11,5 +11,10 @@ public class SceneManager : MonoBehaviour {
         {
             Application.Quit();
         }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
     }
 }
