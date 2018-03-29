d3b8336c9f2955b60070bbbc8a5751aef225f4f5
Vera <schoenigervera@gmail.com>
2018-03-29 17:19:30 +0200

    Menu
    - built most menu Prefabs
    - changed name input to pressing a done button to confirm the name

--- SheepGames/Assets/MainMenu/MainMenuController.cs
+++ SheepGames/Assets/MainMenu/MainMenuController.cs
@@ -1,29 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour {
 
-    public void SetPlayerName(string playerName)
+    public TMP_InputField nameInput;
+    public GameObject nameInputObject;
+
+    public void SetPlayerName()
     {
-        DataCollector.currentPlayer = playerName;
-        DataCollector.CheckForSaveFile(); 
+        DataCollector.currentPlayer = nameInput.text;
+        DataCollector.CheckForSaveFile();
+        nameInputObject.SetActive(false);
     }
 
-    ///DEBUG FUNCTION 
-    ///add this to a button on the main menu to write temporary savegame to console
-    public void ShowDict()
-    {
-        ///players without any scores won't get displayed by this, re-enable comment if needed
-        foreach (KeyValuePair<string, Dictionary<string, int>> kvp in DataCollector.tempPlayerDict)
-        {
-            string player = kvp.Key;
-            //print(player);
-            ICollection coll = kvp.Value;
-            foreach (KeyValuePair<string, int>item in coll)
-            {
-                Debug.Log("Player: " + player + "; Level: " + item.Key + ": " + item.Value);
-            }
-        }
-    }
+
 }
