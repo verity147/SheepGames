f01bc4410de14af8b5a2cdc81b0e08c064a64f27
Vera <schoenigervera@gmail.com>
2018-06-26 11:37:44 +0200

    ObstacleSheep
    - built bounce anim with root motion
    - adjusted collision matrix

---
+++ SheepGames/Assets/JH_ObstacleSheep.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JH_ObstacleSheep : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody2D rigidb;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigidb = GetComponent<Rigidbody2D>();
+        rigidb.isKinematic = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("hit playertrigger");
+            //Vector3 currentPos = transform.position;
+            rigidb.isKinematic = false;
+            //anim.applyRootMotion = false;
+            //transform.position = currentPos;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("hit player");
+            anim.SetTrigger("collision");
+        }
+    }
+
+}
