821117610c1edd699e9fa451a9244d72869d8cfd
Vera <schoenigervera@gmail.com>
2018-07-27 16:43:13 +0200

    PW Start
    - first tests and mockup of fighting sheep for PW

---
+++ SheepGames/Assets/PressOWar/PW_Scripts/PW_SheepMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PW_SheepMovement : MonoBehaviour {
+
+    public Vector2 force;
+
+    private Rigidbody2D rBody;
+
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+	}
+	
+    private void Push()
+    {
+        rBody.AddForce(force, ForceMode2D.Impulse);
+    }
+
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Push();
+        }
+	}
+}
