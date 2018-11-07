3ccd1165c9b087ea7824563b13f1315e79aa194c
Vera <schoenigervera@gmail.com>
2018-11-07 12:39:21 +0100

    Timer
    - build Timer element and animation
    - updated direction sprite
    - tidied up some old sprites

---
+++ SheepGames/Assets/Application/ParticleHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleHandler : MonoBehaviour {
+
+    private ParticleSystem partSys;
+
+    private void Awake()
+    {
+        partSys = GetComponent<ParticleSystem>();
+    }
+
+    private void OnEnable()
+    {
+        RunParticleSystem();
+    }
+
+    public void RunParticleSystem()
+    {
+        if (partSys.isPlaying)
+        {
+            partSys.Stop();
+        }
+        partSys.Play();
+    }
+}
