a530449b1bedb6bcfdb79e7242f22275fa4db65f
Vera <schoenigervera@gmail.com>
2019-03-15 15:00:48 +0100

    Candle Lighting
    - build particle effect foor the candle flame
    - started on light flicker script, not quite working yet, check link in script

---
+++ SheepGames/Assets/PP_LightFlicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PP_LightFlicker : MonoBehaviour {
+
+    public Light myLight;
+
+    [Range(0.1f, 2f)]
+    public float minIntensity = 0.1f;
+
+    [Range(0.1f, 5f)]
+    public float maxIntensity = 2f;
+
+    public float flickerSpeed;
+
+    //file:///C:/Program%20Files/Unity/Editor/Data/Documentation/en/ScriptReference/Mathf.Lerp.html
+
+    private void Update()
+    {
+        if (myLight.intensity<=minIntensity)
+        {
+            print("smol");
+            myLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, flickerSpeed);
+        }
+        else if (myLight.intensity >= maxIntensity)
+        {
+            print("big");
+            myLight.intensity = Mathf.Lerp(maxIntensity, minIntensity, flickerSpeed);
+        }
+    }
+}
