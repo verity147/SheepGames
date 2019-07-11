7293a43d1458353c5392a03467894bbbf69acaf5
Vera <schoenigervera@gmail.com>
2019-07-11 16:09:01 +0200

    Hillrace Start of prototyping
    - built character controller and camera

---
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_Groundcheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HR_Groundcheck : MonoBehaviour
+{
+    private HR_Player player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<HR_Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 15)
+        {
+            player.isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 15)
+        {
+            player.isGrounded = false;
+        }
+    }
+}
---
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HR_Player : MonoBehaviour
+{
+    public float runSpeed = 10f;
+    public float aircontrolSpeed = 10f;
+    public float jumpforce = 10f;
+
+    internal bool isGrounded;
+
+    private Rigidbody2D myRigidbody;
+    private float moveInput;
+    private bool lookRight = true;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        if (isGrounded)
+        {
+            myRigidbody.velocity = new Vector2(moveInput * runSpeed, myRigidbody.velocity.y);
+        }
+        else if (!isGrounded)
+        {
+            myRigidbody.velocity = new Vector2(moveInput * aircontrolSpeed, myRigidbody.velocity.y);
+        }
+
+        if (lookRight == false && moveInput > 0)
+        {
+            Flip();
+        }else if(lookRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            myRigidbody.velocity = Vector2.up * jumpforce;
+        }
+
+    }
+
+    private void Flip()
+    {
+        lookRight = !lookRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
