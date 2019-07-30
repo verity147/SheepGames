da755c93d89a8c9baa7dcac2ecd996a152089c68
Vera <schoenigervera@gmail.com>
2019-07-30 16:08:48 +0200

    Collectables
    - test objects for bonbon collectable
    - made brush for bonbons
    - started scripting collectables
    - gameManager to coordinate scripts

---
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_Bonbon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HR_Bonbon : MonoBehaviour
+{
+    public HR_Gamemanager gameManager;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<HR_Gamemanager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.gameObject.tag == "Player")
+        {
+            gameManager.CollectPoint();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_Gamemanager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HR_Gamemanager : MonoBehaviour
+{
+    //remember to reset this on restart
+    private int pointsCollected = 0;
+
+    internal void CollectPoint()
+    {
+        pointsCollected += 1;
+        //add UI counter
+    }
+}
