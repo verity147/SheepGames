1965ef6ee4614d31c0278d50d97638e044c1b818
Vera <schoenigervera@gmail.com>
2019-11-11 17:56:11 +0100

    Bonbon fancification
    - made bonbons rotate continuously
    - optimized bonbon lighting and material for fanciness
    - built animation for spring & started on logic
    - added fixed waterfall sprites

---
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_Spring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HR_Spring : MonoBehaviour
+{
+
+    public HR_Player player;
+    public float force = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject);
+        if (collision.gameObject.tag=="Player")
+        {
+            print("player");
+
+            //use ApplyJumpForce() in Player script, but find a way to use it until jumpHeight has been reached
+            //edit jumpHeight, AddForce() doesn't work with this rigidbody setup
+            //maybe use jumpHeight + jumpBoost for height value (build boosted vs unboosted jump logic?)
+            GetComponentInChildren<Animator>().SetTrigger("spring");
+        }
+    }
+}
