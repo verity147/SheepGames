2a77ae1f031e657d4c5cac3d18c020b296905b49
Vera <schoenigervera@gmail.com>
2019-11-13 13:32:07 +0100

    Shattering Platform
    - added shattering platform as tileset brush

---
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_ShatterPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HR_ShatterPlatform : MonoBehaviour
+{
+    private Animator anim;
+    private Collider2D coll;
+    internal HR_Player player;
+    private HR_Groundcheck groundcheck;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        groundcheck = player.GetComponentInChildren<HR_Groundcheck>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == groundcheck.gameObject)
+        {
+            anim.SetTrigger("shatter");
+        }
+    }
+
+    public void ToggleCollider()
+    {
+        coll.enabled = !coll.enabled;
+    }
+}
