3825ec82f5d2b8e447aa15a41d1aef2c869c260b
Vera <schoenigervera@gmail.com>
2019-12-06 15:53:12 +0100

    Parallax optimisation & fixes
    - added another parallax layer for clouds
    - improved parallax code to allow changing number of layers from the inspector
    - more levelbuilding
    - fixed bridge collision

--- SheepGames/Assets/Hillrace/HR_Scripts/HR_ParallaxScroll.cs
+++ SheepGames/Assets/Hillrace/HR_Scripts/HR_ParallaxScroll.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 
 public class HR_ParallaxScroll : MonoBehaviour
 {
-    public Renderer background;
-    public Renderer foreground;
     public GameObject mainCam;
-    public float backgroundSpeed = 0.02f;
-    public float foregroundSpeed = 0.06f;
-    public float backgroundHeight = 0.001f;
-    public float foregroundHeight = 0.002f;
 
-    private float offsetX = 0f;
-    private float offsetY = 0f;
-    private float startHeight;
+    public List<HR_ParallaxLayer> parallaxLayers = new List<HR_ParallaxLayer>();
 
-    private void Start()
-    {
-        startHeight = transform.position.y;
-    }
+    private float camOffsetX = 0f;
+    private float camOffsetY = 0f;
 
     private void Update()
     {
-        offsetX = mainCam.transform.position.x;
-        offsetY = mainCam.transform.position.y;
+        camOffsetX = mainCam.transform.position.x;
+        camOffsetY = mainCam.transform.position.y;
 
-        float backgroundOffset = offsetX * backgroundSpeed;
-        float foregroundOffset = offsetX * foregroundSpeed;
-
-        float backgroundYoffset = offsetY * backgroundHeight;
-        float foregroundYoffset = offsetY * foregroundHeight;
-
-        background.material.mainTextureOffset = new Vector2(backgroundOffset, backgroundYoffset);
-        foreground.material.mainTextureOffset = new Vector2(foregroundOffset, foregroundYoffset);
+        foreach(HR_ParallaxLayer layer in parallaxLayers)
+        {
+            float horizOffset = layer.horizontalSpeed * camOffsetX;
+            float vertOffset = layer.verticalSpeed * camOffsetY;
+            layer.rend.material.mainTextureOffset = new Vector2(horizOffset, vertOffset);
+        }
     }
 }
+
+[System.Serializable]
+public class HR_ParallaxLayer
+{
+    public string name;
+    public Renderer rend;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    public HR_ParallaxLayer(string newName, Renderer newRend, float newHorizontalSpeed, float newVerticalSpeed)
+    {
+        name = newName;
+        rend = newRend;
+        horizontalSpeed = newHorizontalSpeed;
+        verticalSpeed = newVerticalSpeed;
+    }
+}
+
+
