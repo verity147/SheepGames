c7d023d01f0775d9e20dd606b149601d5606a4c1
Vera <schoenigervera@gmail.com>
2020-02-12 17:22:55 +0100

    Party Scene
    - improved some fireworks
    - filled out timeline
    - added end text
    - timeline now brings you back to main menu when done

---
+++ SheepGames/Assets/Party/PartyManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyManager : MonoBehaviour
+{
+    public SceneHandler sceneHandler;
+
+    private void OnEnable()
+    {
+        sceneHandler.LoadLevel("04_MainMenu");
+    }
+}
